262549124578eea083e3940469b1acfce4ce78af
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-18 12:10:31 +0530

    Design the basic UI of the editor

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
 public class EditorFormController {
+    public TextArea txtEditor;
+
+    public void mnuItemNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemExit_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemFind_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemReplace_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void mnuItemSelectAll_OnAction(ActionEvent actionEvent) {
+    }
 }
