8975211b6fa14cefea000cc993e98df052f697b2
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-18 19:00:58 +0530

    Add highlight support for the FX TextArea

--- src/controller/EditorFormController.java
+++ src/controller/EditorFormController.java
@@ -2,22 +2,100 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import util.FXUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class EditorFormController {
+    private final List<Index> searchList = new ArrayList<>();
     public TextArea txtEditor;
+    public AnchorPane pneFind;
+    public TextField txtSearch;
+    private int findOffset = -1;
+
+    public void initialize() {
+        pneFind.setVisible(false);
+
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            FXUtil.highlightOnTextArea(txtEditor,newValue, Color.web("yellow", 0.8));
+
+            try {
+                Pattern regExp = Pattern.compile(newValue);
+                Matcher matcher = regExp.matcher(txtEditor.getText());
+
+                searchList.clear();
+
+                while (matcher.find()) {
+                    searchList.add(new Index(matcher.start(), matcher.end()));
+                }
+            } catch (PatternSyntaxException e) {
+
+            }
+        });
+    }
 
     public void mnuItemNew_OnAction(ActionEvent actionEvent) {
+        txtEditor.clear();
+        txtEditor.requestFocus();
     }
 
     public void mnuItemExit_OnAction(ActionEvent actionEvent) {
     }
 
     public void mnuItemFind_OnAction(ActionEvent actionEvent) {
+        pneFind.setVisible(true);
+        txtSearch.requestFocus();
     }
 
     public void mnuItemReplace_OnAction(ActionEvent actionEvent) {
+
     }
 
     public void mnuItemSelectAll_OnAction(ActionEvent actionEvent) {
+        txtEditor.selectAll();
+    }
+
+    public void btnFindNext_OnAction(ActionEvent actionEvent) {
+        if (!searchList.isEmpty()) {
+            if (findOffset == -1) {
+                findOffset = 0;
+            }
+            txtEditor.selectRange(searchList.get(findOffset).startingIndex, searchList.get(findOffset).endIndex);
+            findOffset++;
+            if (findOffset >= searchList.size()) {
+                findOffset = 0;
+            }
+        }
+    }
+
+    public void btnFindPrevious_OnAction(ActionEvent actionEvent) {
+        if (!searchList.isEmpty()) {
+            if (findOffset == -1) {
+                findOffset = searchList.size() - 1;
+            }
+            txtEditor.selectRange(searchList.get(findOffset).startingIndex, searchList.get(findOffset).endIndex);
+            findOffset--;
+            if (findOffset < 0) {
+                findOffset = searchList.size() - 1;
+            }
+        }
+    }
+}
+
+class Index {
+    int startingIndex;
+    int endIndex;
+
+    public Index(int startingIndex, int endIndex) {
+        this.startingIndex = startingIndex;
+        this.endIndex = endIndex;
     }
 }
---
+++ src/util/FXUtil.java
@@ -0,0 +1,128 @@
+package util;
+
+import com.sun.javafx.scene.control.skin.TextAreaSkin;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FXUtil {
+
+    public static void highlightOnTextArea(TextArea textArea, String regex, Color highlight) {
+        Platform.runLater(() -> {
+
+            Text text = (Text) textArea.lookup("Text");
+            Parent parent = textArea.lookup("Text").getParent().getParent();
+            TextAreaSkin skin = (TextAreaSkin) textArea.getSkin();
+
+            try {
+                Field fldChildren = Parent.class.getDeclaredField("children");
+                fldChildren.setAccessible(true);
+                ObservableList<Node> children = (ObservableList<Node>) fldChildren.get(parent);
+                Canvas canvas = getCanvas(children, text);
+                List<Range> searchResult = getSearchResult(text.getText(), regex);
+                repaint(canvas, skin, searchResult, highlight);
+
+                text.layoutBoundsProperty().addListener((observable, oldValue, newValue) -> {
+                    canvas.setWidth(newValue.getWidth());
+                    canvas.setHeight(newValue.getHeight());
+                    repaint(canvas, skin, searchResult, highlight);
+                });
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                // Nothing to do here 😉
+            }
+
+        });
+    }
+
+    private static void repaint(Canvas canvas, TextAreaSkin skin, List<Range> ranges, Color highlight) {
+
+        GraphicsContext ctx = canvas.getGraphicsContext2D();
+        ctx.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        ctx.setFill(highlight);
+
+        for (Range range : ranges) {
+            double x = skin.getCharacterBounds(range.start).getMinX();
+            double y = skin.getCharacterBounds(range.start).getMinY();
+            double width = skin.getCharacterBounds(range.end - 1).getMaxX() - x;
+            double height = skin.getCharacterBounds(range.end - 1).getMaxY() - y;
+
+            ctx.fillRect(x, y, width, height);
+        }
+    }
+
+    private static Canvas getCanvas(ObservableList<Node> children, Text text) {
+        for (Node child : children) {
+
+            if (child instanceof Canvas) {
+                return (Canvas) child;
+            }
+        }
+
+        Canvas canvas = new Canvas(text.getLayoutBounds().getWidth(), text.getLayoutBounds().getHeight());
+        children.add(canvas);
+        canvas.toBack();
+        canvas.setMouseTransparent(true);
+        return canvas;
+    }
+
+    private static List<Range> getSearchResult(String text, String regex) throws Exception {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        List<Range> ranges = new ArrayList<>();
+
+        while (matcher.find()) {
+            ranges.add(new Range(matcher.start(), matcher.end()));
+        }
+
+        return ranges;
+    }
+
+    private static class Range {
+        private int start;
+        private int end;
+
+        public Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public void setStart(int start) {
+            this.start = start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+
+        public void setEnd(int end) {
+            this.end = end;
+        }
+
+        @Override
+        public String toString() {
+            return "Range{" +
+                    "start=" + start +
+                    ", end=" + end +
+                    '}';
+        }
+    }
+
+}
