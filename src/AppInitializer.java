29357106cb68e1fd2ad842c60fb6ac45cee11b2b
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-07-09 13:56:31 +0530

    Finalize the text editor with the preferences api

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 public class AppInitializer extends Application {
 
@@ -18,7 +19,35 @@ public class AppInitializer extends Application {
         Scene mainScene = new Scene(root);
         primaryStage.setScene(mainScene);
         primaryStage.setTitle("Simple Text Editor");
-        primaryStage.setMaximized(true);
+
         primaryStage.show();
+
+        double width = Preferences.userRoot().node("simple-text-editor").getDouble("width", -1);
+        double height = Preferences.userRoot().node("simple-text-editor").getDouble("height", -1);
+
+        if (width == -1 && height == -1){
+            primaryStage.setMaximized(true);
+        }else{
+            primaryStage.setWidth(width);
+            primaryStage.setHeight(height);
+        }
+
+        double xPos = Preferences.userRoot().node("simple-text-editor").getDouble("xPos", -1);
+        double yPos = Preferences.userRoot().node("simple-text-editor").getDouble("yPos", -1);
+
+        if (xPos == -1 && yPos == -1){
+            primaryStage.centerOnScreen();
+        }else{
+            primaryStage.setX(xPos);
+            primaryStage.setY(yPos);
+        }
+
+        primaryStage.setOnCloseRequest(event -> {
+            Preferences.userRoot().node("simple-text-editor").putDouble("xPos", primaryStage.getX());
+            Preferences.userRoot().node("simple-text-editor").putDouble("yPos", primaryStage.getY());
+            Preferences.userRoot().node("simple-text-editor").putDouble("width", primaryStage.getWidth());
+            Preferences.userRoot().node("simple-text-editor").putDouble("height", primaryStage.getHeight());
+        });
+
     }
 }
